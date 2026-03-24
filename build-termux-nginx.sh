416a7d7d97bd44f85e60a37e29fc3ef7e5eecb4c
shengshampoo <shengzh@tutanota.com>
2026-03-24 13:09:51 +0000

    Create build-termux-nginx.sh

---
+++ build-termux-nginx.sh
@@ -0,0 +1,77 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace2
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+export CC="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang"
+export CXX="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang++"
+export AR="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-ar"
+export AS=$CC
+export LD="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/ld"
+export RANLIB="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-ranlib"
+export STRIP="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/llvm-strip"
+export PATH=/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/:$PATH
+export ANDROID_NDK_HOME="/android-ndk-r29"
+export ANDROID_NDK="/android-ndk-r29"
+export ANDROID_NDK_ROOT="/android-ndk-r29"
+
+# nginx
+cd $WORKSPACE
+git clone https://github.com/nginx/nginx
+cd nginx
+./auto/configure --prefix=/usr/local/nginxmm \
+  --with-poll_module --with-file-aio --with-threads \
+  --with-http_ssl_module --with-http_v2_module \
+  --with-http_dav_module --with-http_mp4_module \
+  --with-http_gzip_static_module --with-stream --with-stream_realip_module \
+  --with-stream_ssl_module --with-stream_ssl_preread_module \
+  --with-http_realip_module --with-pcre --with-pcre-jit \
+  --with-openssl-opt=enable-ktls --with-libatomic \
+  --with-cc="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang" \
+  --with-cc-opt='-std=gnu17 -O3 -fno-pie -no-pie -Wno-error -DNGX_QUIC_OPENSSL_API=1' \
+  --with-ld-opt='-static -fno-pie -no-pie -lgcov -lstdc++ -lmodsecurity -lyajl -lxml2 -llmdb -lfuzzy -L/usr/lib/lua5.4 -llua -lcurl -lssl -lcrypto -lcares -lnghttp2 -lidn2 -lpsl -lssh2 -lunistring -lbrotlienc -lbrotlidec -lbrotlicommon -lxslt' \
+  --with-openssl=/$(find / -maxdepth 1 -type d -name "openssl-*" -exec basename {} \;) --with-http_v3_module \
+  --with-http_addition_module --add-module=/ngx_brotli \
+  --add-module=/zstd-nginx-module --add-module=/ngx_http_geoip2_module \
+  --add-module=/nginx-vod-module --add-module=/nginx-http-flv-module \
+  --add-module=/nginx-module-vts --add-module=/ModSecurity-nginx \
+  --add-module=/njs/nginx --with-http_gzip_static_module
+
+make
+make install
+
+# angie
+# https://en.angie.software/angie/docs/development/
+cd $WORKSPACE
+git clone https://git.angie.software/web-server/angie 
+cd angie
+./configure --prefix=/usr/local/angiemm \
+  --with-poll_module --with-file-aio --with-threads \
+  --with-http_ssl_module --with-http_v2_module \
+  --with-http_dav_module --with-http_mp4_module \
+  --with-http_gzip_static_module --with-stream --with-stream_realip_module \
+  --with-stream_ssl_module --with-stream_ssl_preread_module \
+  --with-http_realip_module --with-pcre --with-pcre-jit \
+  --with-openssl-opt=enable-ktls --with-libatomic \
+  --with-cc="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang" \  
+  --with-cc-opt='-std=gnu17 -O3 -fno-pie -no-pie -Wno-error -DNGX_QUIC_OPENSSL_API=1' \
+  --with-ld-opt='-static -fno-pie -no-pie -lgcov -lstdc++ -lmodsecurity -lyajl -lxml2 -llmdb -lfuzzy -L/usr/lib/lua5.4 -llua -lcurl -lssl -lcrypto -lcares -lnghttp2 -lidn2 -lpsl -lssh2 -lunistring -lbrotlienc -lbrotlidec -lbrotlicommon -lxslt' \
+  --with-openssl=/$(find / -maxdepth 1 -type d -name "openssl-*" -exec basename {} \;) --with-http_v3_module \
+  --with-http_addition_module --add-module=/ngx_brotli \
+  --add-module=/zstd-nginx-module --add-module=/ngx_http_geoip2_module \
+  --add-module=/nginx-vod-module --add-module=/nginx-http-flv-module \
+  --add-module=/nginx-module-vts --add-module=/ModSecurity-nginx \
+  --add-module=/njs/nginx --with-http_gzip_static_module
+
+make
+make install
+
+cd /usr/local
+tar vcJf ./nginxmm.tar.xz nginxmm
+tar vcJf ./angiemm.tar.xz angiemm
+
+mv ./[an]*mm.tar.xz /work/artifact/
