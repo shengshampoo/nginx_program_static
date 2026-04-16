adfe723d7a3d522f593ed4b1e81bb268d6c710d3
shengshampoo <shengzh@tutanota.com>
2026-04-16 21:58:23 +0800

    Update build-ngx-addon.sh

--- build-ngx-addon.sh
+++ build-ngx-addon.sh
@@ -16,8 +16,8 @@ git clone https://github.com/vozlt/nginx-module-vts
 git clone https://github.com/owasp-modsecurity/ModSecurity-nginx
 git clone https://github.com/nginx/njs
 
-curl -sL https://github.com/openssl/openssl/releases/download/openssl-3.6.2/openssl-3.6.2.tar.gz | tar x --gzip
-#curl -sL https://github.com/openssl/openssl/releases/download/openssl-4.0.0-beta1/openssl-4.0.0-beta1.tar.gz | tar x --gzip
+#curl -sL https://github.com/openssl/openssl/releases/download/openssl-3.6.2/openssl-3.6.2.tar.gz | tar x --gzip
+curl -sL https://github.com/openssl/openssl/releases/download/openssl-4.0.0-beta1/openssl-4.0.0-beta1.tar.gz | tar x --gzip
 
 cd /
 cd ./ModSecurity-nginx
